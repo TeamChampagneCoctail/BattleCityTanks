affe0b18530d0d87f0f2616747892231fdc8176e
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-08-04 01:49:34 +0300

    Added webStorage implementation for collected data

--- app/app.js
+++ app/app.js
@@ -1,17 +1,65 @@
 $(document).ready(function(){
-    collectLanguagesData();
+    var data;
+
+    if(localStorage){
+        data = getLanguagesData();
+    }
+    else{
+        //exception,error etc
+    }
+
+    console.dir(data);
 });
 
-function collectLanguagesData(){
-    var ajaxFactory = new AjaxFactory();
+function getLanguagesData(){
+    var currentTimestamp = new Date($.now()),
+        lastUpdate = Date.parse(localStorage.getItem('lastModified')),
+        dateDiff = currentTimestamp - lastUpdate,
+        dateDiffMinutes = Math.round(((dateDiff % 86400000) % 3600000) / 60000),
+        languagesData = localStorage.getItem('languagesData');
 
-    var cSharpLanguageData = {
-        success: function(data){
-            $('#test').append(JSON.stringify(data));
+    //todo check storage not result !!!
+    if(isNaN(dateDiffMinutes) || dateDiffMinutes === null){
+        languagesData = executeGithubApiCall();
+    }
+    else{
+        if(dateDiffMinutes < 60){
+
+            languagesData = JSON.parse(localStorage.getItem('languagesData'));
+        }
+    }
+
+    return languagesData;
+}
+
+function executeGithubApiCall(){
+    var ajaxFactory = new AjaxFactory(),
+        languagesData = [],
+        currentDateTime = new Date($.now()),
+        callbackObj = {
+            success: function(data){
+                languagesData.push(JSON.stringify(data));
         }
     };
 
-    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:C#", cSharpLanguageData);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:Java", callbackObj);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:C#", callbackObj);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:GO", callbackObj);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:Javascript", callbackObj);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:C", callbackObj);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:C++", callbackObj);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:Python", callbackObj);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:Ruby", callbackObj);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:Perl", callbackObj);
+    ajaxFactory.getData("https://api.github.com/search/repositories?q=language:PHP", callbackObj);
+
+    //todo improve this *** with $when
+    $(document).ajaxStop(function () {
+        localStorage.setItem('lastModified', currentDateTime);
+        localStorage.setItem('languagesData', JSON.stringify(languagesData));
+    });
+
+    return languagesData;
 }
 
 
