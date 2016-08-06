4ff920c1964f01c76f042785d3baafd30a17aad3
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-08-07 01:55:21 +0300

    Added landing page html, styles, js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
-    // GameEngine.Start();
+    startGameEventListener();
 });
 
+function startGameEventListener(){
+    $("#start-game").on('click', function(e) {
+        e.preventDefault();
+        var hash = this.hash;
+
+
+        $('html, body').animate({
+            scrollTop: $("#canvas-section").offset().top
+        }, 500, function(){
+
+            //start
+            // GameEngine.Start();
+        });
+
+    });
+}
