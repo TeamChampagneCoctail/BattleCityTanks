2d744178cf0777da28e98ad9503059c05a408554
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-08-08 00:12:41 +0300

    Fixed game map drawing and image

--- app/Models/map.js
+++ app/Models/map.js
@@ -5,38 +5,26 @@ var Map= {
     mapObjects: {},
 
     /* Functions */
+
     drawBackgroundImage: function(stage){
         Map.backgroundLayer = new Kinetic.Layer();
 
         var imageObj = new Image();
-        imageObj.src = "https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/leisa_christmas_false_color.png?itok=Jxf0IlS4";
-
-        var backgroundImage = new Kinetic.Rect({
-            x: 0,
-            y: 0,
-            width: stage.getWidth(),
-            height: stage.getHeight(),
-            fillPatternImage: imageObj,
-            fillPatternRepeat: 'repeat-x'
-            //fill: 'yellow'
-        });
-
-
+        imageObj.src = "assets/images/map/map_background.png";
 
         imageObj.onload = function(){
-            backgroundImage.setFillPatternImage(backgroundImage);
-            backgroundImage.fillPatternScaleX(1);
-            backgroundImage.fillPatternScaleY(1);
+            var backgroundImage = new Kinetic.Rect({
+                x: 0,
+                y: 0,
+                width: stage.getWidth(),
+                height: stage.getHeight(),
+                fillPatternImage: imageObj,
+                fillPatternRepeat: 'repeat-x'
+            });
 
+            Map.backgroundLayer.add(backgroundImage);
+            Map.backgroundLayer.draw();
         };
-
-
-       /* var backgroundImage = new Kinetic.Image({
-            image: imageObj
-        });*/
-        Map.backgroundLayer.add(backgroundImage);
-
-
     },
 
     setupMapObjects: function(){
@@ -53,14 +41,16 @@ var Map= {
         Map.mapLayer = new Kinetic.Layer();
         Map.setupMapObjects();
 
+
         // 2. Draw objects
-        // Map.drawBackgroundImage(stage);
         Map.drawBackgroundImage(stage);
         Map.drawMapObjects();
 
         // 3. Add layers to main stage
         stage.add(Map.backgroundLayer);
         stage.add(Map.mapLayer);
+
+        //console.dir(Map.backgroundLayer);
 
         // 4. Draw the map
         Map.mapLayer.draw();
