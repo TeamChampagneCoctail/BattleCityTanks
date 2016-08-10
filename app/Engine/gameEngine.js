8a8ba01981af9f9f66125446ed93191d6443b816
Alex Nestorov <nestorov.alexander@gmail.com>
2016-08-10 23:59:39 +0300

    Add automation enemy move

--- app/Engine/gameEngine.js
+++ app/Engine/gameEngine.js
@@ -21,9 +21,56 @@ var gameEngine = function() {
         // Creating units.
         Map.init(gameStage);
         playerUnit = gameUnitsFactory.createPlayer(playerStartX, playerStartY, playerLayer);
-        let enemyUnit1 = gameUnitsFactory.createEnemy(150, 50, enemiesLayer);
-        let enemyUnit2 = gameUnitsFactory.createEnemy(150, 150, enemiesLayer);
+        let enemyUnit1 = gameUnitsFactory.createEnemy(canvasWidth - 40, 0, enemiesLayer);
+        let enemyUnit2 = gameUnitsFactory.createEnemy(canvasWidth - 40, 240, enemiesLayer);
+        enemyUnit2.sprite.changeAnimation('left');
 
+        // Moving enemy without animation.
+        for (var i = 0; i < 9999; i++) {
+                 if (enemyUnit1.width.startAnimation === 'down') {
+
+                   let y = enemyUnit1.sprite.spriteSheet.getY();
+                   enemyUnit1.sprite.spriteSheet.setY(y + 5);
+                 }else if (enemyUnit1.width.startAnimation === 'right'){
+
+                   let x = enemyUnit1.sprite.spriteSheet.getX();
+                   enemyUnit1.sprite.spriteSheet.setX(x + 5);
+                 }else if (enemyUnit1.width.startAnimation === 'up') {
+
+                   let y = enemyUnit1.sprite.spriteSheet.getY();
+                   enemyUnit1.sprite.spriteSheet.setY(y - 5);
+                 }else if (enemyUnit1.width.startAnimation === 'left') {
+
+                   let x = enemyUnit1.sprite.spriteSheet.getX();
+                   enemyUnit1.sprite.spriteSheet.setX(x - 5);
+                 }
+
+                 if (enemyUnit1.sprite.spriteSheet.getY() === canvasHeight - 40 &&
+                     enemyUnit1.width.startAnimation === 'down') {
+
+                     enemyUnit1.sprite.changeAnimation('left');
+                     enemyUnit1.width.startAnimation = 'left';
+
+                 }else if (enemyUnit1.sprite.spriteSheet.getX() === 0 &&
+                     enemyUnit1.width.startAnimation === 'left'){
+
+                     enemyUnit1.sprite.changeAnimation('up');
+                     enemyUnit1.width.startAnimation = 'up';
+
+                }else if (enemyUnit1.sprite.spriteSheet.getY() === 0 &&
+                     enemyUnit1.width.startAnimation === 'up'){
+
+                     enemyUnit1.sprite.changeAnimation('right');
+                     enemyUnit1.width.startAnimation = 'right';
+
+                }else if (enemyUnit1.sprite.spriteSheet.getX() === canvasWidth - 40 &&
+                     enemyUnit1.width.startAnimation === 'right'){
+
+                     enemyUnit1.sprite.changeAnimation('down');
+                     enemyUnit1.width.startAnimation = 'down';
+
+                }
+        }
         document.addEventListener('keydown', function(ev) {
             let keyCode = ev.keyCode;
             if (keyCode === 37) {
